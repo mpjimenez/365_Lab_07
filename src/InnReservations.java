06a1a8e22e8ed82556b93fd915c4bc71c6301090
dereklance <derekwarnerlance@yahoo.com>
2017-11-18 14:13:33 -0800

    Refactor code and allow user to use environment variables

--- src/InnReservations.java
+++ src/InnReservations.java
@@ -14,23 +14,31 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class InnReservations {
+    public static void testConnection(Connection conn) throws SQLException {
+        String sql = "select * from lab7_rooms limit 1";
+
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                String roomCode = rs.getString("RoomCode");
+                BigDecimal basePrice = rs.getBigDecimal("basePrice");
+                System.out.format("roomCode: %s\nbasePrice: %f\n", roomCode, basePrice);
+            }
+        }
+    }
     public static void main(String[] args) throws SQLException {
-//        try (Connection conn = DriverManager.getConnection(System.getenv("HP_JDBC_URL"),
-//                System.getenv("HP_JDBC_USER"),
-//                System.getenv("HP_JDBC_PW"))) {
-        try (Connection conn = DriverManager.getConnection(
-                args[0], args[1], args[2]
-        )) {
-            String sql = "select * from lab7_rooms limit 1";
-
-            try (Statement stmt = conn.createStatement();
-                 ResultSet rs = stmt.executeQuery(sql)) {
-
-                while (rs.next()) {
-                    String roomCode = rs.getString("RoomCode");
-                    BigDecimal basePrice = rs.getBigDecimal("basePrice");
-                    System.out.format("roomCode: %s\nbasePrice: %f\n", roomCode, basePrice);
-                }
+        if (args.length != 3) {
+            try (Connection conn = DriverManager.getConnection(
+                    System.getenv("HP_JDBC_URL"),
+                    System.getenv("HP_JDBC_USER"),
+                    System.getenv("HP_JDBC_PW"))) {
+                testConnection(conn);
+            }
+        }
+        else {
+            try (Connection conn = DriverManager.getConnection(args[0], args[1], args[2])) {
+                testConnection(conn);
             }
         }
     }
