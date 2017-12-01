9e4a40a9630b0d64180e4303728d6db8147ffff3
Derek Lance <dlance@calpoly.edu>
2017-12-01 15:40:34 -0800

    Finish #3

--- src/InnReservations.java
+++ src/InnReservations.java
@@ -1,21 +1,12 @@
 
 
 import java.math.BigDecimal;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.Date;
+import java.sql.*;
 
+import java.sql.Date;
 import java.text.SimpleDateFormat;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.LinkedHashMap;
+import java.util.*;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.ArrayList;
 
 public class InnReservations {
 
@@ -67,7 +58,7 @@ public class InnReservations {
    }
 
    public static void printMenu() {
-      String line = "Enter the number that cooresponds" +
+      String line = "Enter the number that corresponds" +
                     "to your desired action\n\n";
 
       System.out.println("Rooms and Rates [1]");
@@ -151,7 +142,118 @@ public class InnReservations {
    }
 
    public static void DetailedReservationInfo(String[] envVar) throws SQLException {
+      try (Connection conn = DriverManager.getConnection(System.getenv("HP_JDBC_URL"),
+              System.getenv("HP_JDBC_USER"),
+              System.getenv("HP_JDBC_PW"))) {
 
+
+
+         boolean hasCondition = false;
+         Scanner reader = new Scanner(System.in);
+         System.out.println("RESERVATION LOOKUP (LEAVE ENTRY BLANK TO INDICATE ANY)\n");
+         System.out.println("FIRST NAME: ");
+         String firstName = reader.nextLine();
+         hasCondition = hasCondition || firstName.length() > 0;
+
+         System.out.println("LAST NAME: ");
+         String lastName = reader.nextLine();
+         hasCondition = hasCondition || lastName.length() > 0;
+
+         System.out.println("START DATE: ");
+         String startDate = reader.nextLine();
+         hasCondition = hasCondition || startDate.length() > 0;
+
+         System.out.println("END DATE: ");
+         String endDate = reader.nextLine();
+         hasCondition = hasCondition || endDate.length() > 0;
+
+         System.out.println("ROOM CODE: ");
+         String roomCode = reader.nextLine();
+         hasCondition = hasCondition || roomCode.length() > 0;
+
+         System.out.println("RESERVATION CODE: ");
+         String reservationCode = reader.nextLine();
+         hasCondition = hasCondition || reservationCode.length() > 0;
+
+         String query = "select lab7_rooms.roomName, lab7_reservations.* from lab7_reservations inner join lab7_rooms on room = roomCode";
+         if (hasCondition) {
+            query += " where ";
+         }
+
+         List<Object> params = new ArrayList<>();
+         StringJoiner sj = new StringJoiner(" AND ");
+
+         if (firstName.length() > 0) {
+            if (firstName.contains("%") || firstName.contains("_")) {
+               sj.add("firstName like ?");
+            }
+            else {
+               sj.add("firstName = ?");
+            }
+            params.add(firstName);
+         }
+
+         if (lastName.length() > 0) {
+            if (lastName.contains("%") || lastName.contains("_")) {
+               sj.add("lastName like ?");
+            }
+            else {
+               sj.add("lastName = ?");
+            }
+            params.add(lastName);
+         }
+         if (startDate.length() > 0) {
+            sj.add("CheckIn >= ?");
+            params.add(stringToSqlDate(startDate));
+         }
+         if (endDate.length() > 0) {
+            sj.add("Checkout <= ?");
+            params.add(stringToSqlDate(endDate));
+         }
+         if (roomCode.length() > 0) {
+            if (roomCode.contains("%") || roomCode.contains("_")) {
+               sj.add("room like ?");
+            }
+            else {
+               sj.add("room = ?");
+            }
+            params.add(roomCode);
+         }
+         if (reservationCode.length() > 0) {
+            if (reservationCode.contains("%") || reservationCode.contains("_")) {
+               sj.add("code like ?");
+               params.add(reservationCode);
+            }
+            else {
+               sj.add("code = ?");
+               params.add(Integer.parseInt(reservationCode));
+            }
+         }
+
+         try (PreparedStatement statement = conn.prepareStatement(query + sj)) {
+            int i = 1;
+            for (Object param : params) {
+               statement.setObject(i++, param);
+            }
+
+            try (ResultSet rs = statement.executeQuery()) {
+               System.out.println("MATCHING RESULTS:\n----------------------------------");
+               while (rs.next()) {
+                  System.out.println("ROOM NAME: " + rs.getString("roomName"));
+                  System.out.println("CODE: " + rs.getString("code"));
+                  System.out.println("ROOM: " + rs.getString("room"));
+                  System.out.println("CHECK IN: " + rs.getString("CheckIn"));
+                  System.out.println("CHECK OUT: " + rs.getString("Checkout"));
+                  System.out.println("RATE: " + rs.getDouble("Rate"));
+                  System.out.println("LAST NAME: " + rs.getString("LastName"));
+                  System.out.println("FIRST NAME: " + rs.getString("FirstName"));
+                  System.out.println("ADULTS: " + rs.getInt("adults"));
+                  System.out.println("KIDS: " + rs.getInt("kids") + "\n----------------------------------");
+               }
+               System.out.println();
+            }
+         }
+      }
    }
 
    public static void Revenue(String[] envVar) throws SQLException {
